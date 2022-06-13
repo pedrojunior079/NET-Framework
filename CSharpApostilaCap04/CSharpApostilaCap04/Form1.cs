3930dfb7e796a9bcd8c22a76e167d0ba7dcafb2e
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-06-13 11:31:05 -0300

    Update

---
+++ CSharpApostilaCap04/CSharpApostilaCap04/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpApostilaCap04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valorSaque = 10.0;
+            if(saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque realizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Imnsuficiente");
+            }      
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double saldo = 5.0;
+            double valorSaque = 10.0;
+            if(saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque realziado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }     
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double saldo = 500.0;
+            if(saldo < 0.0)
+            {
+                MessageBox.Show("Vcoe está no negativo!");
+            }
+            else if(saldo < 1000000.0)
+            {
+                MessageBox.Show("Voce é um bom cliente");
+            }
+            else
+            {
+                MessageBox.Show("Voce é milionario!");
+            }
+        }
+    }
+}
---
+++ CSharpApostilaCap3/CSharpApostilaCap3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpApostilaCap3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensagem = "Olá ";
+            string nome = "PedroJr";
+            int idade = 42;
+            MessageBox.Show(mensagem + "meu nome é " + nome + " minha idade é " + idade);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valorDoSaque = 10.0;
+            saldo = saldo - valorDoSaque;
+
+            MessageBox.Show("O saldo da conta após o saque é: " +saldo);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double pi = 3.14;
+            int piQuebrado = (int)pi;
+            MessageBox.Show("piQuebrado = " +piQuebrado);
+        }
+    }
+}
