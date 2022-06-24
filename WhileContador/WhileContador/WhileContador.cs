d2e227362b83117cb3b95fd76a0ba24f37f1c61e
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-06-23 23:34:24 -0300

    Incremento, WhileContador C#

---
+++ WhileContador/WhileContador/WhileContador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhileContador
+{
+    class WhileContador
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;       // inicialização
+                                     
+            while(contador <= 5)    // condição de repetição
+            {
+                Console.WriteLine(contador);
+                contador++;        // incrementa
+            } //fim do while
+        } // fim do Main
+    } // fim da classe WhileContador
+}
