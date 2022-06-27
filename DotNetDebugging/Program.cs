94bab2eaee0b9dd83085d24aab15359149734007
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-06-27 13:39:29 -0300

    Adicao, DotNetDebugging, DotNetDepensdencias, mslearn-dotnet-files, WhileContador

---
+++ DotNetDebugging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+int resultado = Fibonacci(6);
+Debug.WriteLine($"Entering {nameoof(Fibonacci)} method");
+Debug.WriteLine($"We are looking for the {n}th number");
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int soma;
+
+    for(int i = 2; i <= n; i++)
+    {
+        soma = n1 + n2;
+        n1 = n2;
+        n2 = soma;
+        Debug.Assert(soma == 1, $"soma foi 1, n1 foi {n1}, n2 foi {n2}");
+        return n == 0 ? n1 : n2;
+
+    }
+
+    return n == 0 ? n1 : n2;
+}
