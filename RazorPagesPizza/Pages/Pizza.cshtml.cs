1790374588b57b77ee9e5ec5fe4f97b48dc69729
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-06-30 17:15:09 -0300

    RazorPagesPizza

---
+++ RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public List<Pizza> pizzas = new();
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
+        
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+            
+        }
+        
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Pages();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if(pizza.IsGlutenFree)
+               return "Gluten Free";
+            return "Not Gluten Free";   
+        }
+            
+    }
+}
