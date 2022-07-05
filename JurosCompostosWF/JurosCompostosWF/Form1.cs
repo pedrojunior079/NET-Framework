13fe4cd1c991c37d546e7f62a05b236a1917d8d9
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-07-05 15:05:08 -0300

    JurosCompostosWF, SwitchTeste

---
+++ JurosCompostosWF/JurosCompostosWF/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JurosCompostosWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal quantia, principal = (decimal)1000.00;
+            double taxa = .05;
+            string saida;
+
+            saida = "Ano\tQuantia Depositada\n";
+
+            for(int ano = 1; ano <= 10; ano++)
+            {
+                quantia = principal * (decimal) Math.Pow(1.0 + taxa, ano);
+                saida += ano + "\t" + String.Format("{0:C}", quantia) + "\n";
+            }
+            MessageBox.Show(saida, "Juros Compostos",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ SwitchTeste/SwitchTeste/SwitchTeste.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SwitchTeste
+{
+    class SwitchTeste
+    {
+        static void Main(string[] args)
+        {
+            char nota;              // uma nota
+            int aContador = 0,     // número de notas A
+                bContador = 0,     // número de notas B
+                cContador = 0,     // número de notas C
+                dContador = 0,     // núnero de notas D
+                fContador = 0;     // número de notas F
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write("Entre com a letra da nota: ");
+                nota = Char.Parse(Console.ReadLine());
+
+                switch(nota)
+                {
+                    case 'A':     // a nota é A maiusculo
+                    case 'a':     // ou a minúsculo
+                        ++aContador;
+                        break;
+
+                    case 'B':    // a nota é B maiúsculo
+                    case 'b':    // a nota é b minúsculo
+                        ++bContador;
+                        break;
+
+                    case 'C':    // a nota é C maiúsculo
+                    case 'c':    // a nota é c minúsculo
+                        ++cContador;
+                        break;
+
+                    case 'D':    // a nota é D maiúsculo
+                    case 'd':    // a nota é d minúsculo
+                        ++dContador;
+                        break;
+
+                    case 'F':    // a nota é F maiúsculo
+                    case 'f':    // a nota é f minúsculo
+                        ++fContador;
+                        break;
+
+                    default:     // processa todos os outros caracteres
+                        Console.WriteLine("Nota de letra incorreta inserida" +
+                                          "\nNota não adicionada aos totais.");
+                        break;
+                } // fim do switch
+            } // fim do for
+
+            Console.WriteLine("\nOs totais para cada nota de letra são: \nA: {0}" +
+                              "\nB: {1}\nC: {2}\nD: {3}\nF: {4}", aContador, bContador,
+                              cContador, dContador, fContador);
+        } // fim do metodo main
+    } // fim da classe Switchteste
+}
