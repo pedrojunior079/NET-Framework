90d330741d1076a6cc40faf14a6d23ce20316a2c
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-08-05 11:08:59 -0300

    BreakTestWF, DoWhileLoopCS

---
+++ BreakTestWF/BreakTestWF/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreakTestWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string saida = "";
+            int contador;
+
+            for(contador = 1; contador <= 10; contador++)
+            {
+                if (contador == 5)
+                    break;  // pula po codigo restante no laço
+                            // se contador == 5
+
+                saida += contador + " ";
+            } // fim do laco
+
+            saida += "\nSaiu do loop na contagem = " + contador;
+
+            MessageBox.Show(saida, "Demonstrando a instrução break",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } //fim do metodo Main
+    } // fim da classe BreakTest
+}
---
+++ DoWhileLoopCS/DoWhileLoopCS/DoWhileLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoWhileLoopCS
+{
+    class DoWhileLoop
+    {
+        static void Main(string[] args)
+        {
+            static void Main(String[] args)
+            {
+                int contador = 1;
+
+                do
+                {
+                    Console.WriteLine(contador);
+                    contador++;
+                }while(contador <= 5);
+            } // fim do metodo Main
+        } // fim da classe DoWhileLoop
+    }
+}
