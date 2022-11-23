cf7b096056317c67671ccfac6d2f7bb149aaf096
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-11-23 00:07:45 -0300

    ContinueTesteWF e OperadoresLogicosCS

---
+++ ContinueTesteWF/ContinueTesteWF/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContinueTesteWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string output = "";
+            for(int contador = 1; contador <= 10; contador++)
+            {
+                if (contador == 5)
+                    continue;  //pula o codigo restante no laço
+                               //apenas se contador == 5
+                output += contador + "";                
+            }
+
+            output += "\nUsado continue para pular a impressão 5";
+
+            MessageBox.Show(output, "Usando a instrução continue",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ OperadoresLogicosCS/OperadoresLogicosCS/Program.cs
@@ -0,0 +1,52 @@
+﻿//Demonstrando operadores logicos
+
+using System;
+
+namespace OperadoresLogicosCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testando o operador E condicional (&&)
+            Console.WriteLine("Condicional AND (&&)"+
+                "\nfalse && false:" + (false && false)+
+                "\nfalse && true:" + (false && true)+
+                "\ntrue && false:" + (true && false)+
+                "\ntrue && true:" + (true && true));
+
+            //testando o operador OU condicional (||)
+            Console.WriteLine("\n\nCondicional OR (||)"+
+                "\nfalse || false:" + (false || false)+
+                "\nfalse || true:" + (false || true)+
+                "\ntrue || false:" + (true || false)+
+                "\ntrue || true:" + (true || true));
+
+            //testando o operador E logico (&)
+            Console.WriteLine("\n\nLogico AND (&)"+
+                "\nfalse & false:" + (false & false)+
+                "\nfalse & true:" + (false & true)+
+                "\ntrue & false:" + (true & false)+
+                "\ntrue & true:" + (true & true));
+
+            //testando o operador OU logico (|)
+            Console.WriteLine("\n\nLogico OR (|)"+
+                "\nfalse | false:" + (false | false)+
+                "\nfalse | true:" + (false | true)+
+                "\ntrue | false:" + (true | false)+
+                "\ntrue | true:" + (true | true));
+
+            //testando o operador OU exclusivo logico (^)
+            Console.WriteLine("\n\nExclusivo Logico OR (^)" +
+                "\nfalse ^ false:" + (false ^ false) +
+                "\nfalse ^ true:" + (false ^ true) +
+                "\ntrue ^ false:" + (true ^ false) +
+                "\ntrue ^ true:" + (true ^ true));
+
+            //testando o operador NAO logico (!)
+            Console.WriteLine("\n\nExclusivo Logico OR (^)" +
+                "\n!false:" + (!false)+
+                "\n!true:" + (!true));
+        }
+    }
+}
